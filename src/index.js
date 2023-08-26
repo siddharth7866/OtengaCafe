4793888894d9a07795be761db63daf6bac4bced3
siddharth vishen <42781456+siddharth7866@users.noreply.github.com>
2023-08-26 17:21:16 +0530

    Props and components

--- src/index.js
+++ src/index.js
@@ -4,10 +4,93 @@ import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Pizza />
+    <App />
   </React.StrictMode>
 );
 
-function Pizza() {
-  return <h1>Hello</h1>;
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+function Menu() {
+  return (
+    <main className="menu">
+      <h2>Our Menu</h2>
+      <Pizza pizzaName="sid" pizzaImage="pizzas/focaccia.jpg" />
+      <Pizza pizzaName="sid" pizzaImage="pizzas/focaccia.jpg" />
+      <Pizza pizzaName="sid" pizzaImage="pizzas/focaccia.jpg" />
+    </main>
+  );
+}
+function Pizza(props) {
+  return (
+    <div className="pizza">
+      <img src={props.pizzaImage} alt={props.pizzaName} />
+      <h3>{props.pizzaName}</h3>
+
+      <p> "Bread with italian olive oil and rosemary"</p>
+    </div>
+  );
+}
+
+const pizzaData = [
+  {
+    name: "Pizza Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Fast React Pizza Co.</h1>
+    </header>
+  );
+}
+
+function Footer() {
+  return <footer className="footer">We are open till 9 o clock</footer>;
 }
